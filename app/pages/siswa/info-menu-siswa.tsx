e6ddd3c8d10cc8e29253022aafabd387675c7b07
Ade Andriansyah <syahandrianeda@gmail.com>
2026-09-24 15:04:21 +0700

    dad yang ketinggalan

--- app/pages/siswa/info-menu-siswa.tsx
+++ app/pages/siswa/info-menu-siswa.tsx
@@ -1,6 +1,6 @@
 export default function InfoSiswa(){
     return (
-        <div className="border-2 rounded-2xl bg-white min-h-24 w-10/12 mx-auto p-4">
+        <div className="border-2 rounded-2xl bg-white min-h-24 w-10/12 mx-auto p-4 ">
             Info Untuk Siswa di laman Menu
         </div>
     )
